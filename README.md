863cc5d645fab2d5fe287ed6d435ef9e9ea1d7b8
wanziyu <ziyuwan@zju.edu.cn>
2022-04-16 11:37:35 +0800

    update README

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ edge computing ML model inference on Kubernetes.
 
 ## Overview
 Melody uses Deep Q-learning as the base scheduling algorithm, use prometheus to monitor Edge computing resources and input metrics to DQN, and obtain the optimal scheduling strategy.
+
+<h1 align="center">
+    <img src="docs/img/struct.png" alt="logo" width="800">
+</h1>
+
+## Features
+key benefits include:
+- Support auto-scheduling ML inference pods between edge nodes.
+- Continuously balance the computing resources (cpu, memory) on edge nodes.
+- Consider the local model store for online serving jobs.
+- Equipped with DQN reinforcement learning algorithm to obtain the balanced scheduling strategy.
+
+## Get Started
--- controllers/inference_controller.go
+++ controllers/inference_controller.go
@@ -80,16 +80,7 @@ var _ reconcile.Reconciler = &InferenceReconciler{}
 //+kubebuilder:rbac:groups=melody.io.melody.io,resources=inferences,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=melody.io.melody.io,resources=inferences/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=melody.io.melody.io,resources=inferences/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Inference object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+//+kubebuilder:rbac:groups=melody.io.melody.io,resources=pod,verbs=get;update;patch
 func (r *InferenceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.WithValues("Inference", req.NamespacedName)
 
