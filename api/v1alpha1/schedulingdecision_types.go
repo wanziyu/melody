068ef60598d6939461b8b7648940ae4601f58236
wanziyu <ziyuwan@zju.edu.cn>
2022-06-20 11:37:20 +0800

    update SchedulingDecision types

--- api/v1alpha1/schedulingdecision_types.go
+++ api/v1alpha1/schedulingdecision_types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022.
+Copyright 2022. The Melody Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,25 +17,145 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // SchedulingDecisionSpec defines the desired state of SchedulingDecision
 type SchedulingDecisionSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Scheduling algorithm, e.g., A2C.
+	Algorithm AlgorithmSpec `json:"algorithm,omitempty"`
 
-	// Foo is an example field of SchedulingDecision. Edit schedulingdecision_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Maximum number of inferences
+	MaxNumInferences *int32 `json:"maxNumTrials,omitempty"`
+
+	// Parallelism is the number of concurrent inferences.
+	Parallelism *int32 `json:"parallelism,omitempty"`
+
+	// The request template in json format, used for testing against the REST API of target service.
+	RequestTemplate string `json:"requestTemplate,omitempty"`
+
+	// Client Template to trigger the prometheus monitoring client against target service
+	ClientTemplate v1beta1.JobTemplateSpec `json:"clientTemplate,omitempty"`
+
+	// The target service inference needed to be better scheduled
+	ServicePodTemplate corev1.PodTemplate `json:"servicePodTemplate,omitempty"`
+
+	// The maximum time in seconds for a deployment to make progress before it is considered to be failed.
+	ServiceProgressDeadline *int32 `json:"serviceProgressDeadline,omitempty"`
 }
 
 // SchedulingDecisionStatus defines the observed state of SchedulingDecision
 type SchedulingDecisionStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// List of observed runtime conditions for this SchedulingDecision.
+	Conditions []SchedulingCondition `json:"conditions,omitempty"`
+
+	// Current optimal parameters
+	CurrentOptimalScheduling SchedulingResult `json:"currentOptimalTrial,omitempty"`
+
+	// Sampled configurations and the corresponding object values
+	SchedulingResultList []SchedulingResult `json:"trialResultList,omitempty"`
+
+	// Completion time of the scheduling
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+
+	// Start time of the scheduling
+	StartTime *metav1.Time `json:"startTime,omitempty"`
+
+	// List of inference names which are running.
+	RunningInferenceList []string `json:"runningInferenceList,omitempty"`
+
+	// List of inference names which are pending.
+	PendingInferenceList []string `json:"pendingInferenceList,omitempty"`
+
+	// List of inference names which have already failed.
+	FailedInferenceList []string `json:"failedInferenceList,omitempty"`
+
+	// List of inference names which have already succeeded.
+	SucceededInferenceList []string `json:"succeededInferenceList,omitempty"`
+
+	// List of inference names which have been killed.
+	KilledInferenceList []string `json:"killedInferenceList,omitempty"`
+
+	// TrialsTotal is the total number of inference owned by the experiment.
+	InferencesTotal int32 `json:"inferencesTotal,omitempty"`
+
+	// How many inferences have succeeded.
+	InferencesSucceeded int32 `json:"inferencesSucceeded,omitempty"`
+
+	// How many inferences have been killed.
+	InferencesKilled int32 `json:"inferencesKilled,omitempty"`
+
+	// How many inferences are pending.
+	InferencesPending int32 `json:"inferencesPending,omitempty"`
+
+	// How many inferences are running.
+	InferencesRunning int32 `json:"inferencesRunning,omitempty"`
+
+	// How many inferences have failed.
+	InferencesFailed int32 `json:"inferencesFailed,omitempty"`
+}
+
+// SchedulingCondition describes the state of the experiment at a certain point.
+type SchedulingCondition struct {
+	// Type of experiment condition.
+	Type SchedulingConditionType `json:"type"`
+
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+
+	// A description message indicating details about the transition.
+	Message string `json:"message,omitempty"`
+
+	// The last time this condition was updated.
+	LastUpdateTime metav1.Time `json:"lastUpdateTime,omitempty"`
+}
+
+type SchedulingResult struct {
+	// The scheduling target inference instance
+	TargetPod string `json:"targetPod,omitempty"`
+	// The target scheduling edge node for target pod
+	NodeName string `json:"nodeName,omitempty"`
+}
+
+// SchedulingConditionType defines the status of the SchedulingDecision
+type SchedulingConditionType string
+
+const (
+	SchedulingCreated    SchedulingConditionType = "Created"
+	SchedulingRunning    SchedulingConditionType = "Running"
+	SchedulingRestarting SchedulingConditionType = "Restarting"
+	SchedulingSucceeded  SchedulingConditionType = "Succeeded"
+	SchedulingFailed     SchedulingConditionType = "Failed"
+	SchedulingCompleted  SchedulingConditionType = "Completed"
+)
+
+// AlgorithmName is the supported searching algorithms
+type AlgorithmName string
+
+const (
+	RLScheduling  AlgorithmName = "RLScheduling"
+	DQNScheduling AlgorithmName = "DQNScheduling"
+	GridSearch    AlgorithmName = "grid"
+)
+
+// AlgorithmSpec is the specification of Opt. algorithm
+type AlgorithmSpec struct {
+	// The name of algorithm for sampling_client: random, grid, bayesian optimization.
+	AlgorithmName AlgorithmName `json:"algorithmName,omitempty"`
+
+	// The key-value pairs representing settings for sampling_client algorithms.
+	AlgorithmSettings []AlgorithmSetting `json:"algorithmSettings,omitempty"`
+}
+
+// AlgorithmSetting defines the parameters key-value pair of the scheduling algorithm
+type AlgorithmSetting struct {
+	// The name of the key-value pair.
+	Name string `json:"name,omitempty"`
+
+	// The value of the key.
+	Value string `json:"value,omitempty"`
 }
 
 //+kubebuilder:object:root=true
